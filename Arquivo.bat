e22fb2143a942bfaf82fb23bef80c9052ce45844
Nathalya Silva <nathalya.gsn@gmail.com>
2025-09-22 13:25:04 -0300

    feat: menu interativo

--- Arquivo.bat
+++ Arquivo.bat
@@ -1,9 +1,56 @@
-@echo off
-echo Iniciando a instalacao silenciosa do Google Chrome...
+@ECHO OFF
+TITLE Canivete Suiço 
 
-rem Certifique-se de que o caminho e o nome do arquivo estão corretos.
+:menu_principal
+CLS
+ECHO =================================
+ECHO              MENU
+ECHO =================================
+ECHO.
+ECHO   [1] Instalar Softwares
+ECHO.
+ECHO   [0] Sair
+ECHO.
+
+SET /P opcao=Escolha uma opcao: 
+
+IF "%opcao%"=="1" GOTO menu_softwares
+IF "%opcao%"=="0" EXIT
+
+ECHO Opcao Invalida! Pressione qualquer tecla para tentar novamente.
+PAUSE > NUL
+GOTO menu_principal
+
+
+:menu_softwares
+CLS
+ECHO =================================
+ECHO      LISTA DE SOFTWARES
+ECHO =================================
+ECHO.
+ECHO   [1] Instalar Google Chrome
+ECHO.
+ECHO   [9] Voltar ao Menu Principal
+ECHO.
+
+SET /P escolha=Qual software deseja instalar?: 
+
+IF "%escolha%"=="1" GOTO instalar_chrome
+IF "%escolha%"=="9" GOTO menu_principal
+
+ECHO Opcao Invalida! Pressione qualquer tecla para tentar novamente.
+PAUSE > NUL
+GOTO menu_softwares
+
+
+:instalar_chrome
+CLS
+ECHO Iniciando a instalacao silenciosa do Google Chrome...
+
+rem O comando abaixo executa o instalador que esta na pasta "Programas".
 start /wait Programas\ChromeSetup.exe /silent /install
 
-echo.
-echo A instalacao foi concluida.
-pause
+ECHO.
+ECHO A instalacao foi concluida.
+PAUSE
+GOTO menu_softwares
